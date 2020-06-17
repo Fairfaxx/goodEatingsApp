efd4dfca0881f9946cd7a25dda4b5a6722c36dae
Fefe Lemaire <fede.lemaire@gmail.com>
2020-06-16 21:51:46 -0300

    all imgs added and the struct

---
+++ GoodEatingsApp/Model/FoodCategory.swift
@@ -0,0 +1,14 @@
+//
+//  FoodCategory.swift
+//  GoodEatingsApp
+//
+//  Created by Fede Lemaire on 6/16/20.
+//  Copyright © 2020 Fede Lemaire. All rights reserved.
+//
+
+import Foundation
+
+struct FoodCategory{
+    let title: String
+    let imageName: String
+}
---
+++ GoodEatingsApp/Model/RecipeModel.swift
@@ -0,0 +1,15 @@
+//
+//  RecipeModel.swift
+//  GoodEatingsApp
+//
+//  Created by Fede Lemaire on 6/16/20.
+//  Copyright © 2020 Fede Lemaire. All rights reserved.
+//
+
+import Foundation
+
+struct Recipe {
+    var title: String
+    var instructions: String
+    var imageName: String
+}
